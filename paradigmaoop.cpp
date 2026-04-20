cf81edf2df47f5ab831c2b8c25322d9c35c307fd
canzonadzka <canzonadzka@gmail.com>
2026-04-20 11:32:17 +0700

    menghapus dan memindahan kodingan praktikum ke file baru

--- paradigmaoop.cpp
+++ paradigmaoop.cpp
@@ -24,28 +24,3 @@ int main(){
     mhs.printData();
 }
 
-#include <iostream>
-#include <string>
-using namespace std;
-
-class Barang {
-    public :
-    string nama;
-    int jumlah;
-    string kategori;
-    string tanggalproduksi;
-
-void printData(){
-        cout << "Nama: " << nama << endl;
-        cout << "Jumlah : " << jumlah << endl;
-        cout << "Kategori : " << kategori << endl;
-        cout << "Tanggalproduksi: " << tanggalproduksi<< endl;
-    }   
-};
-int main() {
-    Barang elektronik;
-    elektronik.nama ="PC";
-    elektronik.jumlah = 2;
-    elektronik.kategori ="elektronik";
-    elektronik.tanggalproduksi = "20-12-2021";
-}
